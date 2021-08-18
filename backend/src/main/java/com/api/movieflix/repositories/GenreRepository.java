41d877b835946398a936bd0e79115a663bf4f248
Yrllanio Souza <yrlanio@gmail.com>
2021-08-18 10:07:40 -0300

    User

--- backend/src/main/java/com/api/movieflix/repositories/GenreRepository.java
+++ backend/src/main/java/com/api/movieflix/repositories/GenreRepository.java
@@ -2,6 +2,8 @@ package com.api.movieflix.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GenreRepository extends JpaRepository<T, ID>{
+import com.api.movieflix.entities.Genre;
+
+public interface GenreRepository extends JpaRepository<Genre, Long>{
 
 }
